7d35055b4951252d6e6d250254d8ee06b4fad071
briahnloo <brian.liu2021@gmail.com>
2025-10-19 22:04:12 -0700

    Remove vercel.json to use UI settings only

--- frontend/src/components/QuickCompare.tsx
+++ frontend/src/components/QuickCompare.tsx
@@ -97,8 +97,8 @@ export default function QuickCompare({ explanation, onExpand }: Props) {
 
                     {/* Quick Perspective Summaries */}
                     {explanation.perspectives.map((perspective, idx) => {
-                        const firstExcerpt = perspective.representative_excerpts && perspective.representative_excerpts.length > 0 
-                            ? perspective.representative_excerpts[0] 
+                        const firstExcerpt = perspective.representative_excerpts && perspective.representative_excerpts.length > 0
+                            ? perspective.representative_excerpts[0]
                             : null;
                         const firstSource = perspective.sources[0];
 
